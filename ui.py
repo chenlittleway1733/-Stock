216d13cc81cecb96c2f746f3a575c3d12eb8edfc
chenlittleway1733 <littleway1733@gmail.com>
2026-04-28 20:13:42 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -262,7 +262,7 @@ def render_main_page(sidebar_state=None):
     # ==========================================
     # 5. 主畫面開始
     # ==========================================
-    st.markdown("## 📈 台股聯網 AI 投資戰情室")
+    st.markdown("## 📈 WAY AI 投資戰情室")
 
     if st.session_state.fugle_key and not f_ok:
         st.error("🚨 **系統警報**：您輸入的「富果 (Fugle) API Key」驗證失敗！請至左側欄檢查金鑰是否輸入正確。")
