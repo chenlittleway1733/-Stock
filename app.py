a501f1e081247c43fa0564b9de7ea368fad39d4f
chenlittleway1733 <littleway1733@gmail.com>
2026-04-29 09:25:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from utils import init_session_state
 # ==========================================
 # 0. 網頁基本設定
 # ==========================================
-st.set_page_config(page_title="way投資戰情室1.1", layout="wide")
+st.set_page_config(page_title="way投資戰情室1.2", layout="wide")
 st.markdown('<meta name="google" content="notranslate">', unsafe_allow_html=True)
 
 init_session_state()
